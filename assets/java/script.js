11282ef2848a11f0beb794389ce325628ad9c179
ColmSam2319 <colmsammon13@gmail.com>
2022-06-17 15:37:04 +0000

    added java for win lose and draw

--- assets/java/script.js
+++ assets/java/script.js
@@ -8,8 +8,11 @@ const paper_div = document.getElementById ("paper");
 const scissors_div = document.getElementById ("scissors");
 const lizard_div = document.getElementById ("lizard");
 const spock_div = document.getElementById ("spock");
+const results_div = document.getElementById ('results')
 let playerScore = 0;
 let computerScore = 0;
+
+/*java for computer choice*/
 
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock',];
@@ -18,6 +21,13 @@ function getComputerChoice(){
 }
 (getComputerChoice());
 
-function winner(playerChoice, computerChoice){
+/* java for win,lose and draw*/
 
+function win(playerChoice, computerChoice){
+    playerScore++;
+}
+function lost(playerChoice, computerChoice){
+    computerScore++;
+}
+function draw(playerChoice, computerChoice){
 }
